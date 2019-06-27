42ec9742dbeb3e192a09f9c8393ad82e0c68d0e5
Marc Dinh <marcdinh@pop-os.localdomain>
2019-06-27 15:45:40 -0700

    wrapped trt optimizer script

--- tools/tf-trt.py
+++ tools/tf-trt.py
@@ -1,33 +1,46 @@
 #this script accelerates a TensorFlow graph with TensorRT using the TF-TRT optimizer. 
 # Must be run on the target platform as optimizations are GPU specific
-import os
+import os, argparse
 import tensorflow as tf
 import tensorflow.contrib.tensorrt as trt
 from tensorflow.python.framework import graph_io
+
 
 _input = 'input_1'
 _output = 'output_node0'
 outputs = ['lanenet_model_1/enet_backend/instance_seg/pix_embedding_conv/Conv2D','lanenet_model_1/enet_frontend/enet_decode_module/fullconv/Relu']
 
 
-def get_frozen_graph():
-  with tf.gfile.FastGFile('/home/marc/PycharmProjects/LaneNet/model/tusimple_lanenet_enet/enet4/saved_model.pb', "rb") as f:
+def get_frozen_graph(frozen_path):
+  with tf.gfile.FastGFile('frozen_path', "rb") as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
   return graph_def
 
 
-frozen_graph_def = get_frozen_graph()
-trt_graph_def = trt.create_inference_graph(frozen_graph_def,
+def optimize_graph(frozen_path):
+	frozen_graph_def = get_frozen_graph(frozen_path)
+    # We precise the file fullname of our freezed graph
+    absolute_model_dir = "/".join(input_checkpoint.split('/')[:-1])
+    output_graph = "trt_model.pb"
+
+	trt_graph_def = trt.create_inference_graph(frozen_graph_def,
 					outputs,
 					max_batch_size=4,
 					max_workspace_size_bytes=1 << 30,
 					precision_mode='FP16')
-tf.reset_default_graph()
-g = tf.Graph()
-with tf.Session(graph=g) as sess:
-	with g.as_default():
-		tf.import_graph_def(
-  		graph_def=trt_graph_def,
-  		name='')
-	graph_io.write_graph(g, '.', 'trt_frozen.pb', as_text=False)
+	tf.reset_default_graph()
+	g = tf.Graph()
+	with tf.Session(graph=g) as sess:
+		with g.as_default():
+			tf.import_graph_def(
+  			graph_def=trt_graph_def,
+  			name='')
+		graph_io.write_graph(g, absolute_model_dir, output_graph, as_text=False)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_path", type=str, default="", help="frozen model to export")
+    args = parser.parse_args()
+
+optimize_graph(args.model_model_path)
